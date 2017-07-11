f4ab21f8d8716b34b4bc197505c4e772b1be136f
15017541 <15017541@myrp.edu.sg>
2017-07-11 14:56:13 +0800

    Initial commit

---
+++ app/src/main/java/android/myapplicationdev/com/googlemap/MainActivity.java
@@ -0,0 +1,138 @@
+package android.myapplicationdev.com.googlemap;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static java.security.AccessController.getContext;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3;
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                fm.findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                LatLng poi_CausewayPoint = new LatLng(1.436065, 103.786263);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_CausewayPoint,
+                        15));
+                UiSettings ui = map.getUiSettings();
+                ui.setZoomControlsEnabled(true);
+                ui.setCompassEnabled(true);
+
+                // show your current location
+                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
+                    map.setMyLocationEnabled(true);
+                } else {
+                    Log.e("GMap - Permission", "GPS access has not been granted");
+                }
+
+                map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker) {
+                        Toast.makeText(MainActivity.this, marker.getTitle(),Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+                });
+
+            }
+        });
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    LatLng north = new LatLng(1.4484105, 103.7767974);
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(north,
+                            15));
+                    Marker northMarker = map.addMarker(new
+                            MarkerOptions()
+                            .position(north)
+                            .title("HQ - North")
+                            .snippet("Block 333, Admiralty Ave 3, 765654 Operating hours: 10am-5pm\n" +
+                                    "Tel:65433456\n")
+                            .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+                }
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    LatLng central = new LatLng(1.3507722, 103.8700355);
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(central,
+                            15));
+                    Marker centralMarker = map.addMarker(new
+                            MarkerOptions()
+                            .position(central)
+                            .title("Central")
+                            .snippet("Block 3A, Orchard Ave 3, 134542 \n" +
+                                    "Operating hours: 11am-8pm\n" +
+                                    "Tel:67788652\n")
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                }
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    LatLng east = new LatLng(1.3451042, 103.9200074);
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(east,
+                            15));
+                    Marker eastMarker = map.addMarker(new
+                            MarkerOptions()
+                            .position(east)
+                            .title("East")
+                            .snippet("Block 555, Tampines Ave 3, 287788 \n" +
+                                    "Operating hours: 9am-5pm\n" +
+                                    "Tel:66776677\"\n")
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+                }
+            }
+        });
+
+
+
+    }
+}
